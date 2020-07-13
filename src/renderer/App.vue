cd9984218aa1a625fc4fd1697b18bb0a3ba01da0
holbrook <wanghaikuo@gmail.com>
2020-07-13 21:09:10 +0800

    add element ui and menu demo

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <Test></Test>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Test from './components/TestElementUI.vue'
+  
   export default {
+    components:{
+      Test,
+    },
     name: 'monotrade-frontend'
   }
 </script>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,6 +3,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui' //element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+Vue.use(ElementUI) //使用elementUI
+
+
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
