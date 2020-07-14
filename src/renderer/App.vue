4c8d449294567953f97feda973a7fc49157da63f
Holbrook <wanghaikuo@tsinghua.org.cn>
2020-07-14 15:10:29 +0800

    add a demo component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
     <Test></Test>
+    <!--3、在模板中使用kebab-case 两种都可以-->
+    <demoPop></demoPop> 
+    <demo-pop></demo-pop> 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
   import Test from './components/TestElementUI.vue'
+  import demoPop from './components/demoPop.vue'
+
+
   //拖拽功能的组件vue-grid-layout
   import VueGridLayout from 'vue-grid-layout';
   
   export default {
     components:{
       Test,
+      demoPop,
       GridLayout: VueGridLayout.GridLayout,
       GridItem: VueGridLayout.GridItem,
     },
---
+++ src/renderer/components/demoPop.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>我是一个demoPop组件</h2>
+    </div>
+</template>
