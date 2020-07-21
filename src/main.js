5e511ada2b234ff0fc1896da3ae2a61e7c708270
holbrook <wanghaikuo@gmail.com>
2020-07-21 21:44:41 +0800

    tmp socket.io

--- src/main.js
+++ src/main.js
@@ -13,6 +13,27 @@ Vue.use(dataV);
 //import { borderBox1 } from '@jiaminghi/data-view'
 //Vue.use(borderBox1)
 
+//全局引入socket.io
+//npm install vue-socket.io --save
+//npm install socket.io-client --save
+import VueSocketIO from 'vue-socket.io'
+//import SocketIO from "socket.io-client"
+
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: 'http://localhost:1221',
+    // vuex: {
+    //     store,
+    //     actionPrefix: 'SOCKET_',
+    //     mutationPrefix: 'SOCKET_'
+    // },
+    options: { path: "/my-app/" } //Optional options
+}))
+//socket可以与vuex结合使用
+//import store from'./yourstore'
+//Vue.use(VueSocketio,socketio('http://socketserver.com:1923'), store);
+
+
 // 按需引入vue-awesome图标
 import Icon from 'vue-awesome/components/Icon';
 import 'vue-awesome/icons/chart-bar.js';
