0811bae2b6d2c8ff4e113d30da6b759a7df5acf6
holbrook <wanghaikuo@gmail.com>
2020-07-26 23:13:29 +0800

    TODO: vue component

--- src/components/market/orderBook.vue
+++ src/components/market/orderBook.vue
@@ -31,3 +31,33 @@
     </q-markup-table>
   </div>
 </template>
+
+
+
+
+<script>
+export default {
+  name: '五档行情',
+  data() {
+    return {
+      
+    };
+  },
+  //Prop 是你可以在组件上注册的一些自定义 attribute,  使用时可以用html 属性，或者用 :xxx 绑定
+  // prop 可以是复杂对象
+  props: ['symbol'],
+  mixins: [echartMixins],
+  mounted() {
+    this.draw();
+  },
+  methods: {
+  },
+  destroyed() {
+    window.onresize = null;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
